f93c80aed4f2d539677e9fb2c435587be7923cc6
keren <kv5171@bs.amalnet.k12.il>
2022-02-22 21:01:34 +0200

    .

--- app/src/main/java/com/example/ex123/EmployeeActivity.java
+++ app/src/main/java/com/example/ex123/EmployeeActivity.java
@@ -55,7 +55,7 @@ public class EmployeeActivity extends AppCompatActivity {
         else if(!goodId(idString)) { // not good id
             Toast.makeText(EmployeeActivity.this, "Wrong employee ID syntax", Toast.LENGTH_SHORT).show();
         }
-        else if(checkInDB(phoneString, Employee.PHONE) || checkInDB(idString, Employee.EMPLOYEE_ID))
+        else if(HelperFunc.checkInDB(phoneString, Employee.PHONE, hlp) || HelperFunc.checkInDB(idString, Employee.EMPLOYEE_ID, hlp))
         {
             Toast.makeText(EmployeeActivity.this, "Data is in the db already!", Toast.LENGTH_SHORT).show();
         }
@@ -72,22 +72,6 @@ public class EmployeeActivity extends AppCompatActivity {
             db.insert(Employee.TABLE_EMPLOYEE, null, cv);
             db.close();
         }
-    }
-
-    private boolean checkInDB(String param, String paramType)
-    {
-        int crsrLen = 0;
-
-        db=hlp.getReadableDatabase();
-        crsr = db.query(Employee.TABLE_EMPLOYEE, new String[]{paramType}, paramType+"=?", new String[]{param}, paramType+"=?", null, null, null);
-
-        crsr.moveToFirst();
-        crsrLen = crsr.getColumnCount();
-        crsr.close();
-        db.close();
-
-        // if there is no elements returned - the param not in the db!
-        return crsrLen != 0;
     }
 
     private boolean goodId(String id)
---
+++ app/src/main/java/com/example/ex123/HelperFunc.java
@@ -0,0 +1,22 @@
+package com.example.ex123;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class HelperFunc {
+    public static boolean checkInDB(String param, String paramType, HelperDB hlp)
+    {
+        int crsrLen = 0;
+
+        SQLiteDatabase db=hlp.getReadableDatabase();
+        Cursor crsr = db.query(Employee.TABLE_EMPLOYEE, new String[]{paramType}, paramType+"=?", new String[]{param}, paramType+"=?", null, null, null);
+
+        crsr.moveToFirst();
+        crsrLen = crsr.getCount();
+        crsr.close();
+        db.close();
+
+        // if there is no elements returned - the param not in the db!
+        return crsrLen != 0;
+    }
+}
